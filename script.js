34437c721d25a52b2156921822a4117b4c657ba3
Abigael <abigaelreyes924@gmail.com>
2025-08-21 14:30:59 +0800

    [add] added script.js file and add buttons functionalities

---
+++ script.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    alert("Hello! This is an alert message.");
+}
+
+function myFunctionOne() {
+    document.getElementById('work-title-one').style.color = "blue";   
+}
+
+function myFunctionTwo() {
+    console.log("myFunctionTwo called");
+    document.getElementById('bio-about-one').style.fontSize = "30px";   
+}
